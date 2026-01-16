140f03607c91c1981bbf99a60c988a06c908f4fa
Terry Jumah <Jehovahjay@gmail.com>
2026-01-16 00:27:20 +0000

    Add UserProfile Card using Props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
+import UserProfile from './components/UserProfile';
 import Footer from './components/Footer';
 import './App.css'
 
@@ -10,6 +11,7 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
       <Footer />
     </>
   )
