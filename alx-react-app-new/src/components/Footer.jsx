144e4289975c31f5901254216413e9c4ec106335
Terry Jumah <Jehovahjay@gmail.com>
2026-01-25 22:59:33 +0000

    Add alx-react-app-new folder

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+function Footer() {
+  return (
+    <footer>
+      <p style={{
+        background: "blue",
+        color: "white",
+        textAlign: "center",
+        padding: "10px"
+      }}>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
